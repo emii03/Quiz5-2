cbc21996e7d5f11b900c40910f3b24a3d1568056
Emily Rosales <>
2021-04-27 04:18:29 +0000

    Initial commit

---
+++ quiz5-2-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const int SIZE=10;
+  int numbers[10] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
+  int diff[10];
+  int sum=0;
+
+  for (int i=0; i<10; i++){
+    sum += numbers[1];
+
+  }
+
+  
+ 
+}
